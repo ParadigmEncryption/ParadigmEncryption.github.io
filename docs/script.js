601180264344fb52b9034d6434f91d36853d0b1c
Paradigm <ParadigmEncryption@gmail.com>
2019-03-20 21:52:19 -0500

    to-do-list added

---
+++ docs/script.js
@@ -0,0 +1,67 @@
+var input = document.getElementById("userinput");
+var enterButton = document.getElementById("enter");
+var ul = document.querySelector("ul");
+var deleteButton = document.querySelector("button.delete");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function addToList() {
+    var newItem = document.createElement("li");
+    var newButton = document.createElement("button");
+    ul.appendChild(newItem);
+    newItem.appendChild(newButton);
+    newButton.appendChild(document.createTextNode("Delete"));
+    newItem.appendChild(document.createTextNode(input.value));
+    newItem.classList.add("list");
+    newButton.classList.add("delete");
+    input.value = "";
+}
+
+
+// function addToList() {
+//     var newItem = document.createElement("li");
+//     newItem.appendChild(document.createTextNode(input.value));
+//     ul.appendChild(newItem);
+//     newItem.classList.add("list");
+//     var newButton = document.createElement("button");
+//     newButton.classList.add("delete");
+//     newItem.appendChild(newButton);
+//     input.value = "";
+// }
+
+function removeFromList() {
+}
+
+function deleteItem(e) {
+    if(e.target.matches("button.delete")) {
+        e.target.classList.toggle("done");
+        var parent = document.getElementById("listParent");
+        var child = e.target.parentElement;
+        parent.removeChild(child);
+    }
+}
+
+function addListAfterClick() {
+    if(inputLength() > 0) {
+        addToList();
+    }
+}
+
+function addListAfterKeypress(e) {
+    if(inputLength() > 0 && e.keyCode === 13) {
+        addToList();
+    }
+}
+ 
+function crossOffList(e) {
+    if(e.target.matches("li.list")) {
+        e.target.classList.toggle("done");
+    }
+}
+
+ul.addEventListener("click", deleteItem);
+ul.addEventListener("click", crossOffList);
+enterButton.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
